0792e5dd5f2ef163ceaea95082831edb4e218cb9
unknown <naikmahesh.naik@gmail.com>
2021-02-20 23:24:26 +0530

    update in calculate daily empwage and add part time

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,13 +1,35 @@
 public class EmpWageBuilderUC1 {
 public static void main(String[] args) {
-        //Constants
-        int IS_FULL_TIME = 1;
-        //computation
-           double empcheck = Math.floor(Math.random() *10) % 2;
-        if (empcheck == IS_FULL_TIME)
-                System.out.println("Employee is Present");
-        else
-                System.out.println("Employee is Abscent");
+			int perHr =20;
+			int dailywageFull = 0;
+			int dailywagepart = 0;
+
+		System.out.println("Welcome to Daily Employee wage");
+
+        for ( int i=0; i<=20; i++ )
+			{
+           int empcheck = (int) (Math.random() *10) % 3;
+        switch (empcheck)
+			{
+				case 1:
+				System.out.println("Full time Employee is Present");
+        dailywageFull = dailywageFull + perHr * 8;
+				break;
+
+				case 2:
+                System.out.println("part time Employee is present");
+			dailywagepart = dailywagepart + perHr * 8;
+				break;
+				
+				default:
+					System.out.println("Employee is Abscent");
+
 
         }
+}
+				System.out.println("Full time Employee wage:" + dailywageFull);
+				System.out.println("part time Employee wage:" + dailywagepart);
+				System.out.println("Total Employee wage" +(dailywageFull + dailywagepart));
+}
+
 }
